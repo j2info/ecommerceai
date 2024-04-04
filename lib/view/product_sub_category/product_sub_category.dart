35d55600ede08b4557555f82173da613ebd18057
AthiraaAthi <athiraa2612@gmail.com>
2024-04-04 11:33:38 +0530

    created screens

---
+++ lib/view/product_sub_category/product_sub_category.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProductSubCategory extends StatelessWidget {
+  const ProductSubCategory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
