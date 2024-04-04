df2a9a467bd4b9bfb84b09c875eea936ef0298c3
User <dhichu64@gmail.com>
2024-04-04 14:56:17 +0530

    modified

---
+++ lib/userscreens/language.dart
@@ -0,0 +1,136 @@
+import 'dart:async';
+import 'package:ecommerceai/userscreens/login.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopee Splash Screen',
+      home: LanguageSelectionScreen(),
+    );
+  }
+}
+
+class LanguageSelectionScreen extends StatefulWidget {
+  @override
+  _LanguageSelectionScreenState createState() => _LanguageSelectionScreenState();
+}
+
+class _LanguageSelectionScreenState extends State<LanguageSelectionScreen> {
+  String? selectedLanguage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 70, left: 90), // Add vertical padding to the title
+            child: Row(
+              children: [
+                Icon(Icons.language,size: 30,),
+                SizedBox(width: 10),
+                Text(
+                  'Language',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                languageContainer('Hindi', 'हिंदी', Colors.red),
+                SizedBox(height: 10),
+                languageContainer('English', 'English', Colors.red),
+                SizedBox(height: 10),
+                languageContainer('Arabic', 'عربى', Colors.red),
+                SizedBox(height: 10),
+                languageContainer('Malayalam', 'മലയാളം', Colors.red),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 20.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => LoginPage())); // Navigate to next screen or perform action
+                  },
+                  child: Text(
+                    'Continue',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget languageContainer(String language, String languageInNative, Color radioColor) {
+    return Container(
+      width: 400,
+      height: 70,
+      margin: EdgeInsets.symmetric(vertical: 35),
+      padding: EdgeInsets.all(10),// Add spacing between containers
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        border: Border.all(color: Colors.grey, width: 2), // Change border color to red
+      ),
+      child: Row(
+        children: [
+          Theme(
+            data: ThemeData(
+              unselectedWidgetColor: Colors.red, // Change the default color of the radio button
+            ),
+            child: Radio(
+
+              value: language,
+              groupValue: selectedLanguage,
+              onChanged: (value) {
+                setState(() {
+                  selectedLanguage = value.toString(); // Convert value to string
+                });
+              },
+              activeColor: radioColor,
+            ),
+          ),
+          SizedBox(width: 40), // Adjust the width as needed for spacing
+          Expanded(
+            child: Text(
+              languageInNative,
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(width: 20), // Adjust the width as needed for additional spacing
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/userscreens/login.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'otp.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopee Login',
+      theme: ThemeData.light(), // Set the theme to light
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(height: 50), // Add space from top
+          Center(
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset(
+                      'images/image2.png', // Replace this with the path to your image icon
+                      width: 40, // Adjust the width of the image
+                      height: 50, // Adjust the height of the image
+                    ),
+                    SizedBox(width: 8), // Adjust the spacing between image and text
+                    Text(
+                      'SHOPEE',
+                      style: TextStyle(
+                        color: Colors.red[700],
+                        fontSize: 42,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 70),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 50.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Login', // Sub-heading
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Please enter your mobile number', // Description
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: TextField(
+                    keyboardType: TextInputType.phone, // Set keyboard type to phone
+                    decoration: InputDecoration(
+                      hintText: 'Mobile Number', // Placeholder text
+                      hintStyle: TextStyle(color: Colors.grey[400]), // Placeholder color
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey[400]!), // Border color
+                      ), // Add border
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 35.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => OTPScreen()),
+                        );
+                        // Add functionality for continue button
+                      },
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red,
+                        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      child: Text(
+                        'Continue',
+                        style: TextStyle(fontSize: 18,color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'or',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  TextButton(
+                    onPressed: () {
+                      // Add functionality for signup button
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 30.0),
+                      child: Text(
+                        'Sign Up?',
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
