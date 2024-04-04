190aff99ab9d420bb06865cb1352a5fb72a9d261
AthiraaAthi <athiraa2612@gmail.com>
2024-04-04 14:53:06 +0530

    completed cart screen

--- lib/view/cart_screen/cart_screen.dart
+++ lib/view/cart_screen/cart_screen.dart
@@ -1,12 +1,20 @@
 import 'package:ecommerceai/utils/color_constant/color_constant.dart';
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class CartScreen extends StatelessWidget {
+class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
+
+  @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  List<String> Numbers = ["1", "2", "3", "4"];
+  String dropDownValue = "1";
 
   @override
   Widget build(BuildContext context) {
@@ -43,61 +51,283 @@ class CartScreen extends StatelessWidget {
           children: [
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Container(
-                color: Colors.amber,
-                height: 200,
-                width: 400,
-                child: Row(
-                  children: [
-                    Container(
-                      height: 180,
-                      width: 180,
-                      color: Colors.blueAccent,
-                    ),
-                    SizedBox(
-                      width: 15,
-                    ),
-                    Expanded(
-                      child: Container(
-                        height: 150,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Noise Smart Watch With Bluetooth Calling",
-                              textAlign: TextAlign.justify,
-                            ),
-                            SizedBox(
-                              height: 15,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                RatingBar.builder(
-                                  unratedColor: Colors.grey,
-                                  initialRating: 4,
-                                  minRating: 1,
-                                  direction: Axis.horizontal,
-                                  itemCount: 5,
-                                  itemSize: 23,
-                                  // itemPadding: EdgeInsets.symmetric(
-                                  //   horizontal: 1,
-                                  // ),
-                                  itemBuilder: (context, index) => Icon(
-                                    Icons.star,
-                                    color: ColorConstant.DefRed,
+              child: Material(
+                elevation: 8,
+                child: Container(
+                  decoration:
+                      BoxDecoration(color: ColorConstant.white, boxShadow: [
+                    BoxShadow(
+                      color: ColorConstant.Black,
+                    )
+                  ]),
+                  height: 200,
+                  width: 400,
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 180,
+                        width: 180,
+                        color: Colors.blueAccent,
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 150,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Noise Smart Watch With Bluetooth Calling",
+                                style: GoogleFonts.poppins(
+                                    textStyle:
+                                        TextStyle(fontWeight: FontWeight.w600)),
+                                textAlign: TextAlign.justify,
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  RatingBar.builder(
+                                    unratedColor: Colors.grey,
+                                    initialRating: 4,
+                                    minRating: 1,
+                                    direction: Axis.horizontal,
+                                    itemCount: 5,
+                                    itemSize: 23,
+                                    // itemPadding: EdgeInsets.symmetric(
+                                    //   horizontal: 1,
+                                    // ),
+                                    itemBuilder: (context, index) => Icon(
+                                      Icons.star,
+                                      color: ColorConstant.DefRed,
+                                    ),
+                                    onRatingUpdate: (index) {},
                                   ),
-                                  onRatingUpdate: (index) {},
+                                  Text("\$ 1,099")
+                                ],
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Container(
+                                height: 30,
+                                width: 70,
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: ColorConstant.DefRed)),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Text(
+                                      "Qty: ",
+                                      style: GoogleFonts.poppins(
+                                          textStyle: TextStyle(
+                                              fontSize: 13,
+                                              fontWeight: FontWeight.bold)),
+                                    ),
+                                    DropdownButton(
+                                      value: dropDownValue,
+                                      items:
+                                          Numbers.map<DropdownMenuItem<String>>(
+                                              (String value) {
+                                        return DropdownMenuItem<String>(
+                                          value: value,
+                                          child: Text(value),
+                                        );
+                                      }).toList(),
+                                      onChanged: (String? value) {
+                                        setState(() {
+                                          dropDownValue = value!;
+                                        });
+                                      },
+                                    ),
+                                  ],
                                 ),
-                                Text("\$ 1,099")
-                              ],
-                            ),
-                          ],
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Text(
+                    "Price Details",
+                    style: GoogleFonts.poppins(
+                      textStyle: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Item",
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16,
+                          ),
                         ),
                       ),
-                    )
-                  ],
-                ),
+                      Text(
+                        "\$1,599",
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Discount",
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "-\$500",
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Delivery Charge",
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Free Delivery",
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Total Amount",
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "\$1,099",
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Divider(
+                    color: Color.fromARGB(255, 214, 212, 212),
+                    thickness: 2,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "\$1,099",
+                          style: GoogleFonts.poppins(
+                            textStyle: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Container(
+                            height: 40,
+                            width: 130,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: ColorConstant.DefRed),
+                            child: Center(
+                                child: Text(
+                              "Buy Now",
+                              style: GoogleFonts.poppins(
+                                  textStyle: TextStyle(
+                                      fontSize: 17,
+                                      color: ColorConstant.white,
+                                      fontWeight: FontWeight.bold)),
+                            )),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
               ),
             )
           ],
